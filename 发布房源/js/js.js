8577f745465e96c6c7d65bd4f9db31f481547ac9
gbt869128710 <869128710@qq.com>
2017-04-19 11:12:50 +0800

    rental
    refer

--- 发布房源/js/js.js
+++ 发布房源/js/js.js
@@ -3,19 +3,29 @@ $(function () {
     var just = /^([1-9][0-9]*){1,3}$/; //不包括0在内的正整数
     var wrong = /^[1-9]d*|0$/; //包括0在内的正整数
     var js;
+    var status1 = 1;
+    var status2 = 1;
+    var status3 = 1;
+    var status4 = 1;
+    var status5 = 1;
+    var status6 = 1;
+    var status7 = 1;
+    var status8 = 1;
     $(".dsnt").on('input change', function () {
         var js = $(".dsnt").val();
         if (js == '' || !zc.test(js)) {
             $(".name .false").show();
             $(".name .true").hide();
+            status1 = 1;
         } else {
             $(".name .true").show();
             $(".name .false").hide();
+            status1 = 0;
         }
     }) //检测是否是中文
     var list = ["雅居乐花园", "御水湾花园", "金陵尚府", "鸿意星城", "中航樾府", "三山街", "芳草园", "瑞金北村"];
 
-    $(".dsnt").on('input change', function () {
+    $(".dsnt").on('input', function () {
         var js = $(".dsnt").val();
 
         if (js != '') {
@@ -58,9 +68,11 @@ $(function () {
         if (js == '' || !wrong.test(js1) || !wrong.test(js2) || !wrong.test(js3) || !wrong.test(js4) || js <= 0) {
             $(".house .false").show();
             $(".house .true").hide();
+            status2 = 1;
         } else {
             $(".house .true").show();
             $(".house .false").hide();
+            status2 = 0;
         }
     }) //户型必须填正整数，可以是0，但不能都是0。
     var money = /^(([1-9][0-9]*)|(([0]\.\d{1,2}|[1-9][0-9]*\.\d{1,2})))$/; //大于0的两位小数
@@ -69,9 +81,11 @@ $(function () {
         if (js == '' || !money.test(js)) {
             $(".area .false").show();
             $(".area .true").hide();
+            status3 = 1;
         } else {
             $(".area .true").show();
             $(".area .false").hide();
+            status3 = 0;
         }
     })
     //检测面积要大于0的两位小数且不为空。
@@ -80,9 +94,11 @@ $(function () {
         if (js == '' || !just.test(js)) {
             $(".rent .false").show();
             $(".rent .true").hide();
+            status4 = 1;
         } else {
             $(".rent .true").show();
             $(".rent .false").hide();
+            status4 = 0;
         }
     })
     //检测租金要大于0的正整数且不为空。
@@ -94,12 +110,15 @@ $(function () {
         if (val == '') {
             $(".phone .false").html("号码不为空！").show();
             $(".phone .true").hide();
+            status5 = 1;
         } else if (!zz.test(val)) {
             $(".phone .false").html("请输入正确格式的手机号！").show();
             $(".phone .true").hide();
+            status5 = 1;
         } else {
             $(".phone .true").show();
             $(".phone .false").hide();
+            status5 = 0;
         }
     })
     //检测手机号码是否正确
@@ -107,9 +126,11 @@ $(function () {
         if ($("#select2").find('option:selected').text() == "区属" || $("#select3").find('option:selected').text() == "板块") {
             $(".plate .false").show();
             $(".plate .true").hide();
+            status6 = 1;
         } else {
             $(".plate .false").hide();
             $(".plate .true").show();
+            status6 = 0;
         }
 
     })
@@ -118,9 +139,11 @@ $(function () {
         if ($("#select4").find('option:selected').text() == "选择卧室内容" || $("#select5").find('option:selected').text() == "选择限制条件") {
             $(".rentout .false").show();
             $(".rentout .true").hide();
+            status7 = 1;
         } else {
             $(".rentout .false").hide();
             $(".rentout .true").show();
+            status7 = 0;
         }
 
     })
@@ -143,10 +166,12 @@ $(function () {
         if ($("input[name='checkbox']").is(':checked')) {
             $(".deposit .false").hide();
             $(".deposit .true").show();
+            status8 = 0;
         } else {
 
             $(".deposit .false").show();
             $(".deposit .true").hide();
+            status8 = 1;
         }
 
     }) //检测至少“checkbox”有一个勾选了
@@ -168,10 +193,14 @@ $(function () {
         $("input[name='checkbox']:checked").each(function () {
             arr.push($(this).val());
         });
-        var val = dsnt + ' ' + select2 + ' ' + select3 + ' ' + frame1 + ' ' + frame2 + ' ' + frame3 + ' ' + frame4 + ' ' + radio + ' ' + select4 + ' ' + select5 + ' ' + dsnt1 + ' ' + dsnt2 + ' ' + arr + ' ' + mobile;
+        //        var val = dsnt + ' ' + select2 + ' ' + select3 + ' ' + frame1 + ' ' + frame2 + ' ' + frame3 + ' ' + frame4 + ' ' + radio + ' ' + select4 + ' ' + select5 + ' ' + dsnt1 + ' ' + dsnt2 + ' ' + arr + ' ' + mobile;
+        //
+        //        $("textarea").html("").val(val);
+        $("textarea").html(
+            "小区名称：" + dsnt + "\n" + "区属板块：" + select2 + " " + select3 + "\n" + "户型：" + frame1 + "室" + " " + frame2 + "厅" + " " + frame3 + "卫" + " " + frame4 + "阳台" + "\n" + "出租形式：" + radio + " " + select4 + " " + select5 + " " + "\n" + "面积：" + dsnt1 + " " + "\n" + "租金：" + dsnt2 + " " + "\n" + "付款方式：" + arr + " " + "\n" + "手机号码：" + mobile
 
-        $("textarea").html("").val(val);
 
+        );
     }) //信息汇总
     $(".upload").on("click", function () {
         var file = $(".file").val();
@@ -185,6 +214,12 @@ $(function () {
         $(".print").hide();
         $(".upload").show();
     }) //关闭图片
-
+    $(".btn").on("click", function () {
+        if (status1 == 0 && status2 == 0 && status3 == 0 && status4 == 0 && status5 == 0 && status6 == 0 && status7 == 0 && status8 == 0) {
+            location.href = "http://www.baidu.com"
+        } else {
+            $(".refer").html("您填写的信息有误")
+        }
+    })
 
 })
